668e409b88dc294a82af275aa9b8b931f667e1ee
Anzhela Adannikova <anzhelaadannikova@gmail.com>
2025-05-10 12:12:20 +0300

    generics

--- src/02-lesson/02-generics.ts
+++ src/02-lesson/02-generics.ts
@@ -30,20 +30,52 @@ getFirstElement<User>([
 
 /* 2 */
 
-function shuffle(array) {
-  return array.soft(() => Math.random() - 0.5);
+function shuffle<T>(array: T[]): T[] {
+  return array.sort(() => Math.random() - 0.5);
 }
 
-const mixedNums = shuffle([1, 2, 3, 4]);
-const mixedWord = shuffle(['apple', 'banana', 'cherry']);
+const mixedNums = shuffle<number>([1, 2, 3, 4]);
+const mixedWord = shuffle<string>(['apple', 'banana', 'cherry']);
 
 /* 3 */
 
-function saveToStorage(key, value) {
+function saveToStorage<T>(key: string, value: T) {
   localStorage.setItem(key, JSON.stringify(value));
 }
 
-saveToStorage('user', 'Jacob Peterson');
-saveToStorage('click', 8);
+saveToStorage<string>('user', 'Jacob Peterson');
+saveToStorage<number>('click', 8);
 
-function loadFromStorage(key) {}
+function loadFromStorage<T>(key: string): T | null {
+  const item = localStorage.getItem(key);
+  if (item !== null) {
+    return JSON.parse(item) as T;
+  }
+  return null;
+}
+
+const user = loadFromStorage<string>('user');
+const click = loadFromStorage<number>('click');
+
+// const result = JSON.parse("hello") as string;
+
+/* 4 */
+
+function max<T>(array: T[], selector: (item: T) => number): T {
+  return array.reduce((prev, curr) =>
+    selector(curr) > selector(prev) ? curr : prev
+  );
+}
+
+interface Product {
+  name: string;
+  price: number;
+}
+
+const products: Product[] = [
+  { name: 'Laptop', price: 1000 },
+  { name: 'Phone', price: 800 },
+];
+
+const mostExpensive = max(products, p => p.price);
+// mostExpensive = { name: 'Laptop', price: 1000 }
